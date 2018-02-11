19ab47d68d36c5053afe2f9100e2fd4178d1e28f
bfayers <bfayers@users.noreply.github.com>
2018-02-11 14:16:48 +0000

    multi item only if the stock changes mode

--- check.py
+++ check.py
@@ -1,26 +1,85 @@
 import requests
 from pushbullet import Pushbullet
 import json
+import re
 
-def check_stock(sku):
+
+def check_stock(sku,stock):
     r = requests.get("https://uk.webuy.com/product.php?sku={}".format(sku))
-    if "buy this item" in r.text:
-        return True
-    else:
-        return False
+    rt = r.text
+    if "buy this item" in rt and stock == "no":
+        #print("In stock, new known")
+        return "isNK"
+    elif "buy this item" in rt and stock == "yes":
+        #print("In stock, pre known")
+        return "isPK"
+    elif "buy this item" not in rt and stock == "yes":
+        #print("Was in stock, now out, new known")
+        return "wisNoNK"
+    elif "buy this item" not in rt and stock == "no":
+        #print("Not in stock, pre known")
+        return "nisPK"
 
 with open("config.json") as json_cfg:
     json_cfg = json.load(json_cfg)
     pb_key = json_cfg['pb_api_key']
-    cex_item_list = json_cfg['item_list']
-    cex_item_list = cex_item_list.split(';')
-
+    items = json_cfg['items']['skus']
+    stocks = json_cfg['items']['stocks']
 
 pb = Pushbullet(pb_key)
 
-for i in cex_item_list:
-    if (check_stock(i)):
-        url = "https://uk.webuy.com/product.php?sku={}".format(i)
-        pb.push_note("CEX Stock Alert", "Item Detected In Stock {}".format(url))
-    else:
+stocked = []
+notStocked = []
+
+for i in items:
+    stock = stocks[items.index(i)]
+    result = check_stock(i, stock)
+    reS = "(?<=\<title\>)(.*)(?= - CeX \(UK\): - Buy, Sell, Donate\<\/title\>)"
+    itemName = requests.get("https://uk.webuy.com/product.php?sku={}".format(i))
+    itemName = itemName.text
+    try:
+        itemName = re.search(reS, itemName).group(0)
+    except:
+        itemName = "Couldn't Parse Name"
+    if result == "isNK":
+        #In stock, new known
+        #print("{} is now in stock".format(i))
+        pb.push_note("CEX Stock Alert", "Item \"{}\" Detected In Stock https://uk.webuy.com/product.php?sku={}".format(itemName, i))
+        stocked.append(i)
+    elif result == "isPK":
+        #In stock, pre known
         pass
+    elif result == "wisNoNK":
+        #Was in stock, now out, new known
+        #print("{} is now out of stock".format(i))
+        pb.push_note("CEX Stock Alert", "Item \"{}\" Detected Out Of Stock https://uk.webuy.com/product.php?sku={}".format(itemName, i))
+        notStocked.append(i)
+    elif result == "nisPK":
+        #Not in stock, pre known
+        pass
+
+#Make the json to re-write :)
+
+itemsJson = {"skus":[],"stocks":[]}
+
+for i in items:
+    itemsJson['skus'].append(i)
+    if i in stocked:
+        #Item is now stocked
+        itemsJson['stocks'].append("yes")
+    elif i not in stocked and i in notStocked:
+        #Item is not stocked
+        itemsJson['stocks'].append("no")
+    else:
+        oldState = stocks[items.index(i)]
+        itemsJson['stocks'].append(oldState)            
+
+#print(itemsJson)
+
+#Lets Write
+with open('config.json', 'r+') as json_cfg:
+    json_cfgW = json.load(json_cfg)
+    json_cfgW['items'] = itemsJson
+    json_cfg.seek(0)
+    json.dump(json_cfgW, json_cfg, indent=4)
+    json_cfg.truncate()
